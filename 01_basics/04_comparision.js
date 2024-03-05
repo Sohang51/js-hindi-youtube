6e5ed86bfc11b065557aab4d32cb6fdf3a8752ce
Sohang51 <139610574+Sohang51@users.noreply.github.com>
2024-03-05 03:59:04 +0000

    Notes Added

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -8,14 +8,14 @@
 // console.log("2" > 1);
 // console.log("02" > 1);
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+console.log(null > 0); //false
+console.log(null == 0); //false
+console.log(null >= 0); //true(because comparison operator converts null into 0)
 
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined < 0);
+console.log(undefined > 0);  //false
+console.log(undefined == 0); //false
+console.log(undefined < 0);//false
 
-// === 
+// ===(strict check)
 
-console.log("2" === 2);
+console.log("2" === 2); //(as its strict check, it will check datatype also and will not convert string to no.)
--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,5 +1,4 @@
 //  Primitive
-
 //  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
 
 const score = 100
@@ -17,9 +16,13 @@ console.log(id === anotherId);
 // const bigNumber = 3456543576654356754n
 
 
+/*
+Primitive data types in JavaScript, such as numbers, strings, booleans, null, and undefined, are typically stored directly in the execution context's stack memory when they are assigned to variables. This is because primitive values have a fixed size and are immutable, so they can be efficiently managed on the stack.
+
+Non-primitive data types, also known as reference types, include objects, arrays, functions, and other objects created using constructors. These data types are typically stored in the heap memory. When you create a variable of a non-primitive type, what's stored in the variable is a reference (or pointer) to the memory location where the actual data is stored in the heap. This reference is what's stored in the stack memory.
+*/
 
 // Reference (Non primitive)
-
 // Array, Objects, Functions
 
 const heros = ["shaktiman", "naagraj", "doga"];
